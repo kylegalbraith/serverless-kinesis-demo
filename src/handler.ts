126082c042c2d03601fbf65a3ce853167076fec9
Kyle Galbraith <kyle.galbraith459@gmail.com>
2019-11-10 13:04:44 -0800

    WIP for test function

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,17 @@
-import { KinesisStreamHandler } from 'aws-lambda';
+import { KinesisStreamHandler, Handler, Context } from 'aws-lambda';
+import { Kinesis } from "aws-sdk";
 import 'source-map-support/register';
 
 export const ingestStream: KinesisStreamHandler = async (event, _context) => {
   event.Records.forEach(record => {
     console.log(record.kinesis.data);
   });
+}
+
+export const feedStream: Handler = async (event, _context: Context) => {
+  const stream = process.env.KINESIS_STREAM;
+  const client = new Kinesis({region: 'us-east-1'});
+  while(_context.getRemainingTimeInMillis() > 30) {
+    
+  }
 }
